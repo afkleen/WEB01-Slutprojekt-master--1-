94482d3d0e039ef29e8d98144556651e72ef0edd
afkleen <vilhelm.melkeraaenafkleen@skola.taby.se>
2023-05-25 14:13:07 +0200

    BEDDDDDÖDMNINGNNNNN

--- WEB01-Slutprojekt-master/app.js
+++ WEB01-Slutprojekt-master/app.js
@@ -45,7 +45,6 @@ function closeModal(modal) {
   overlay.classList.remove("active");
 }
 
-// Get the hamburger menu icon and the navigation menu
 const menuIcon = document.querySelector(".menu-icon");
 const navMenu = document.querySelector(".nav-menu");
 
